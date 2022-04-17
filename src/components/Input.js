396da7c449d4793221cc58c0b2b23094723674a3
otto <5285@mbcpeermade.com>
2022-04-17 23:07:07 +0530

    States defined

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TextField } from '@mui/material'
 import {Button} from '@mui/material'
 import "../style/App.css"
 function Input() {
+    const [ ip , setIp ] = useState( "" );
+    function handleInput( e ){
+        let value = e.target.value;
+        setIp( value )
+    }
+    const inputProps = {
+        onChange:handleInput
+    }
     return (
         <div className='input-ip'>
             <TextField
-                id="outlined-basic" sx={{ input: { color: 'white' }, label: { color: '#8400ff' } }} label="Enter IP Here" variant="outlined" color="secondary" />
+                id="outlined-basic" inputProps={ inputProps } value={ ip } sx={{ input: { color: 'white' }, label: { color: '#8400ff' } }} label="Enter IP Here" variant="outlined" color="secondary" />
             <Button variant="contained" className='Button' color="success">
                 Get Location
             </Button>
